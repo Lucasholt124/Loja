fc6797e55a55ca056df09579e46a12a124b16b70
Lucasholt124 <lucasholt2021@gmail.com>
2025-08-07 20:49:32 -0300

    Strip version

--- app/(store)/webhook/route.ts
+++ app/(store)/webhook/route.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 // Crie a instância do Stripe com sua Secret Key
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2025-07-30.basil",
 });
 
 // Lista de eventos que você quer tratar
